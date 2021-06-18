66416648b88bc7bddd0c94b25ec6b332cac8ac73
diemquyynh <quynhn2772@gmail.com>
2021-06-18 13:44:21 -0700

    LAB2

---
+++ fundamental_practice.py
@@ -0,0 +1,22 @@
+print("hello world") # prints hello world in temrinal 
+name = "Lebron" # string 
+age = 35  # integer
+salary = 37.44 # float
+plays_basketball = True # boolean
+jersey_number = "23" # string 
+
+food = "sushi"
+x = 50 # rolls of sushi
+
+print(name) # print variable 
+print(type(name)) #  print variable type
+
+print(age) 
+print(type(age))
+
+print(salary)
+print(type(salary))
+
+print(plays_basketball)
+print(type(plays_basketball))
+
